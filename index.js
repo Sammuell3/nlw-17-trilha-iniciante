c2d7299b9d93cba8f72ed0370bc98540c648228c
Sammuell3 <samuelsongsytlbry@gmail.com>
2024-09-13 20:14:49 -0300

    Variavel dentro e fora do contexto funcionando

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Imposto é roubo!
+// Fazendo que as duas mensagens funcionem
 const mensagem = 'Imposto é roubo!'
-// {} é um bloco de codigo
-// O escopo de uma variavel é o local onde a variavel é declarada
-// Qualquer variavel declarada dentro de um bloco de codigo, so vai funcionar dentro do bloco de codigo
+
 {
     const mensagem = "Imposto é bom!"
+    console.log(mensagem)
+
 }
-// A variavel declarada fora do bloco de codigo, so vai funcionar fora do bloco de codigo
-// A menssagem que vai aparecer vai ser a que está fora do bloco de codigo
+
 console.log(mensagem)
