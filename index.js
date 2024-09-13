41027250191ed264923782cf68961c7c6e079037
Sammuell3 <samuelsongsytlbry@gmail.com>
2024-09-13 20:09:33 -0300

    Ponto de vista

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const mensagem = 'Imposto é roubo!'
 // {} é um bloco de codigo
 // O escopo de uma variavel é o local onde a variavel é declarada
+// Qualquer variavel declarada dentro de um bloco de codigo, so vai funcionar dentro do bloco de codigo
 {
-    mensagem = "Imposto é roubo!"
+    const mensagem = "Imposto é bom!"
 }
-
+// A variavel declarada fora do bloco de codigo, so vai funcionar fora do bloco de codigo
+// A menssagem que vai aparecer vai ser a que está fora do bloco de codigo
 console.log(mensagem)
-// Não ira funcionar pois o escopo da variavel é limitado ao bloco de codigo
