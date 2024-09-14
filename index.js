e36a4c5c72cfb431f47863717a2443685d586f07
Sammuell3 <samuelsongsytlbry@gmail.com>
2024-09-14 12:41:50 -0300

    Entendendo while com switch case

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // Estrutura de repetição
 function start() {
-    let count = 1
-    // Enquanto a condição for verdadeira, o bloco de código será executado
-    while (count <= 10) {
-        // Se a condição for falsa, o bloco de código não será executado
-        console.log(count)
-        // Incremento
-        count++
-    };
+    while (true) {
+        // enquanto true, o loop vai rodar
+        // se a opção for cadastrar, vai cadastrar
+           // Vai ficar rodando até que o usuário digite "sair"
+        // se a opção for listar, vai listar
+        // se a opção for sair, vai sair
+        let opcao = "sair"
+        switch(opcao) {
+            case "Vamos cadastrar":
+                console.log("Vamos cadastrar")
+                break;
+            case "Listar":
+                console.log("vamos listar")
+                break;
+            case "Sair":
+             return
+        }
+    }
 }
 
-start() // 1 2 3 4 5 6 7 8 9 10
+start() 
