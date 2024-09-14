13ef9dedef2fb60eda3612713591c6f58fb3093d
Sammuell3 <samuelsongsytlbry@gmail.com>
2024-09-14 11:35:21 -0300

    Usando arrow function dentro de um objeto

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 // Objetos são uma coleção de dados que podem conter dados e funções
 let metas = {
     value: "Ler um livro todo mes",
-    checkbox: false
+    checkbox: false,
+    //Se colocarmos a função dentro do objeto, ela se torna um método
+    isChecked: () => {
+        console.log("Meta criada com sucesso!")
+    }
 }
-// para acessar o valor de um objeto, usamos o ponto
-console.log(metas.value)
-
-// para acessar o valor de um objeto, usamos o ponto
-console.log(metas.checkbox)
+// function
+// Isso é uma arrow function
+// Arrow function é uma função que não tem nome
+const criarMeta = () => {}
