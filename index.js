bb24a3512bea6dd1db1dcfaa0302d6124ec37d60
Sammuell3 <samuelsongsytlbry@gmail.com>
2024-09-14 11:25:18 -0300

    Aprendendo objetos e acessando seus valores

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // arrays, objetos
-let metas = ["samuca", "iai"]
-// concatenando arrays
-console.log(metas[0] + " , " + metas[1])
+// Objetos são uma coleção de dados que podem conter dados e funções
+let metas = {
+    value: "Ler um livro todo mes",
+    checkbox: false
+}
+// para acessar o valor de um objeto, usamos o ponto
+console.log(metas.value)
+
+// para acessar o valor de um objeto, usamos o ponto
+console.log(metas.checkbox)
